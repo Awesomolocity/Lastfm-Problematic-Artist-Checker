d27fe9c2e8469e520b2b35377bf3bc44fb348cdc
Sheldon Corcoran <awesomolocity@gmail.com>
2021-11-29 18:21:08 -0800

    Will work without needing to completely reload the page each time.

--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         LastFM Problematic Artist Checker
 // @namespace    https://github.com/Awesomolocity/Lastfm-Problematic-Artist-Checker
 // @downloadURL  https://github.com/Awesomolocity/Lastfm-Problematic-Artist-Checker/raw/master/index.user.js
-// @version      1.1.0
+// @version      1.2.0
 // @description  Checks LastFM profiles for any problematic artists. It's worth remembering that a non-zero number doesn't necessarily mean the user in question is a nazi / nazi sympathizer. But it's still worth keeping an eye on it.
 // @author       Awesomolocity
 // @match        https://www.last.fm/user/*
@@ -54,7 +54,6 @@
 		
 		return getArtistsFromPage(artistsURL, '.link-block-target');
 	}
-	
 	function getArtistsFromTopAlbums(){
 		const username = getUsername();
 		const albumsURL = 'https://www.last.fm/user/'+ username +'/library/albums';
@@ -67,6 +66,7 @@
 		
 		return getArtistsFromPage(tracksURL, '.chartlist-artist');
 	}
+	
 	async function getArtists(){
 		return [...
 			new Set(
@@ -80,25 +80,49 @@
 		];
 	}
 	
-	const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS70_P5rBBdArG539fGuRO3zS_Wt2Pm71_FeMwjQGBG-8iqGq9apAGMJuahBuMnRN_KpzPVc6R_lKSq/pub?output=csv';
-	const request = new Request(sheetURL);
-	fetch(request).then(response => {
-		if(response.status === 200){
-			return response.text();
+	function main(){
+		const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS70_P5rBBdArG539fGuRO3zS_Wt2Pm71_FeMwjQGBG-8iqGq9apAGMJuahBuMnRN_KpzPVc6R_lKSq/pub?output=csv';
+		const request = new Request(sheetURL);
+		fetch(request).then(response => {
+			if(response.status === 200){
+				return response.text();
+			}
+			else{
+				throw new Error('Something went wrong getting the data from Google! :c')
+			}
+		}).then(async response => {
+			const artists = response.split('\r\n');
+			artists.shift(); //First row is a header, so lets remove that
+			const problematicArtists = await getArtists();
+			
+			const problematicArtistCount = problematicArtists.filter(artist => artists.includes(artist)).length;
+			
+			const span = document.createElement('span');
+			span.innerText = ' • ' + problematicArtistCount + ' problematic artist' + (problematicArtistCount === 1 ? '' : 's');
+			
+			document.querySelector('.header-scrobble-since').after(span);
+		});
+	}
+	
+	main();
+	
+	const targetNode = document.getElementById('content');
+	const config = {
+		childList: true
+	}
+	
+	const callback = function(mutationsList, observer){
+		for(const mutation of mutationsList){
+			if(mutation.addedNodes.length){
+				if(document.URL.indexOf('https://www.last.fm/user/') !== -1){
+					if(document.URL.split('https://www.last.fm/user/')[1].indexOf('/') === -1){
+						main();
+					}
+				}
+			}
 		}
-		else{
-			throw new Error('Something went wrong getting the data from Google! :c')
-		}
-	}).then(async response => {
-		const artists = response.split('\r\n');
-		artists.shift(); //First row is a header, so lets remove that
-		const problematicArtists = await getArtists();
-		
-		const problematicArtistCount = problematicArtists.filter(artist => artists.includes(artist)).length;
-		
-		const span = document.createElement('span');
-		span.innerText = ' • ' + problematicArtistCount + ' problematic artist' + (problematicArtistCount === 1 ? '' : 's');
-		
-		document.querySelector('.header-scrobble-since').after(span);
-	});
+	}
+	
+	const observer = new MutationObserver(callback);
+	observer.observe(targetNode, config);
 })();
