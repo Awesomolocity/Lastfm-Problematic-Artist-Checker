da6fc60dbe9ba17e3682dc978f55793d8b88c3e4
Sheldon Corcoran <awesomolocity@gmail.com>
2021-11-29 17:09:06 -0800

    Data importing is better (although slower), readme added, switched to semvar

---
+++ README.md
@@ -0,0 +1,17 @@
+# Last.FM Problematic Artist Checker
+
+A userscript that will check a last.fm user profile for any problematic artists. (AKA: Nazi / Nazi sympathizer / related questionable content) **It is worth noting that having scrobbles for problematic artists does not mean the user is also problematic.**
+
+## Installation
+
+1. Make sure you have user scripts enabled in your browser (these instructions refer to the latest versions of the browser):
+
+    * [Tampermonkey](https://www.tampermonkey.net/) (proprietary)
+    * [Violentmonkey](https://violentmonkey.github.io/get-it/)
+    * [Greasemonkey](https://addons.mozilla.org/firefox/addon/greasemonkey/)
+
+2. [Click this link to install the userscript](https://github.com/Awesomolocity/Lastfm-Problematic-Artist-Checker/raw/main/index.user.js)
+
+## Updating
+
+The script is set to automatically update (theoretically). You can check for updates from within the Greasemonkey or Tampermonkey menu, or click the installation link again to get an update. If you ever find yourself running an outdated version, you can remove the script and reinstall.
--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         LastFM Problematic Artist Checker
 // @namespace    https://github.com/Awesomolocity/Lastfm-Problematic-Artist-Checker
 // @downloadURL  https://github.com/Awesomolocity/Lastfm-Problematic-Artist-Checker/raw/master/index.user.js
-// @version      1.0
+// @version      1.1.0
 // @description  Checks LastFM profiles for any problematic artists. It's worth remembering that a non-zero number doesn't necessarily mean the user in question is a nazi / nazi sympathizer. But it's still worth keeping an eye on it.
 // @author       Awesomolocity
 // @match        https://www.last.fm/user/*
@@ -13,50 +13,68 @@
 (function() {
 	'use strict';
 	
-	function getArtistsFromRecentScrobbles(){
-		return [... //Convert the set to an array
-			new Set( //Sets are nice because they force uniqueness
-				[...document.querySelectorAll('#recent-tracks-section .chartlist-artist')].map(
-					chartlistArtist => chartlistArtist.innerText
+	function HTMLToDocument(htmlString){
+		return new DOMParser().parseFromString(htmlString, 'text/html');
+	}
+	
+	function getUsername(){
+		return document.querySelector('.header-title').innerText;
+	}
+	
+	function getArtistsFromPage(href, selector){
+		const request = new Request(href);
+		return fetch(request).then(response => {
+			if(response.status === 200){
+				return response.text();
+			}
+			else{
+				throw new Error('Something went wrong getting the data from Last.FM! :c')
+			}
+		}).then(response => {
+			const libraryPage = HTMLToDocument(response);
+			return [... //Convert the set to an array
+				new Set( //Sets are nice because they force uniqueness
+					[...libraryPage.querySelectorAll(selector)].map(
+						artist => artist.textContent.trim()
+					)
 				)
-			)
-		];
+			];
+		});
+	}
+	
+	function getArtistsFromRecentScrobbles(){
+		const username = getUsername();
+		const libraryURL = 'https://www.last.fm/user/'+ username +'/library';
+		
+		return getArtistsFromPage(libraryURL, '.chartlist-artist');
 	}
 	function getArtistsFromTopArtists(){
-		return [...
-			new Set(
-				[...document.querySelectorAll('#top-artists .link-block-target')].map(
-					topArtists => topArtists.innerText
-				)
-			)
-		];
+		const username = getUsername();
+		const artistsURL = 'https://www.last.fm/user/'+ username +'/library/artists';
+		
+		return getArtistsFromPage(artistsURL, '.link-block-target');
 	}
+	
 	function getArtistsFromTopAlbums(){
-		return [...
-			new Set(
-				[...document.querySelectorAll('#top-albums .grid-items-item-aux-block')].map(
-					topAlbumsArtist => topAlbumsArtist.innerText
-				)
-			)
-		];
+		const username = getUsername();
+		const albumsURL = 'https://www.last.fm/user/'+ username +'/library/albums';
+		
+		return getArtistsFromPage(albumsURL, '.chartlist-artist');
 	}
 	function getArtistsFromTopTracks(){
-		return [...
-			new Set(
-				[...document.querySelectorAll('#top-tracks .chartlist-artist')].map(
-					chartlistArtist => chartlistArtist.innerText
-				)
-			)
-		];
+		const username = getUsername();
+		const tracksURL = 'https://www.last.fm/user/'+ username +'/library/tracks';
+		
+		return getArtistsFromPage(tracksURL, '.chartlist-artist');
 	}
-	function getArtists(){
+	async function getArtists(){
 		return [...
 			new Set(
 				[].concat(
-					getArtistsFromRecentScrobbles(),
-					getArtistsFromTopArtists(),
-					getArtistsFromTopAlbums(),
-					getArtistsFromTopTracks()
+					await getArtistsFromRecentScrobbles(),
+					await getArtistsFromTopArtists(),
+					await getArtistsFromTopAlbums(),
+					await getArtistsFromTopTracks()
 				)
 			)
 		];
@@ -71,14 +89,15 @@
 		else{
 			throw new Error('Something went wrong getting the data from Google! :c')
 		}
-	}).then(response => {
+	}).then(async response => {
 		const artists = response.split('\r\n');
 		artists.shift(); //First row is a header, so lets remove that
+		const problematicArtists = await getArtists();
 		
-		const problematicArtistCount = getArtists().filter(artist => artists.includes(artist)).length;
+		const problematicArtistCount = problematicArtists.filter(artist => artists.includes(artist)).length;
 		
 		const span = document.createElement('span');
-		span.innerText = ' • '+ problematicArtistCount + ' problematic artist'+ (problematicArtistCount===1?'':'s');
+		span.innerText = ' • ' + problematicArtistCount + ' problematic artist' + (problematicArtistCount === 1 ? '' : 's');
 		
 		document.querySelector('.header-scrobble-since').after(span);
 	});
