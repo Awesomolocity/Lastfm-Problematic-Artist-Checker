7f2462201a1bb03a0e0a0f4dfaa591e3a0e28ae3
Sheldon Corcoran <awesomolocity@gmail.com>
2021-12-01 19:07:21 -0800

    Make sure script only runs on profile pages

--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         LastFM Problematic Artist Checker
 // @namespace    https://github.com/Awesomolocity/Lastfm-Problematic-Artist-Checker
 // @downloadURL  https://github.com/Awesomolocity/Lastfm-Problematic-Artist-Checker/raw/master/index.user.js
-// @version      1.3.1
+// @version      1.3.2
 // @description  Checks LastFM profiles for any problematic artists. It's worth remembering that a non-zero number doesn't necessarily mean the user in question is a nazi / nazi sympathizer. But it's still worth keeping an eye on it.
 // @author       Awesomolocity
 // @match        https://www.last.fm/*
@@ -80,27 +80,29 @@
 	}
 	
 	function main(){
-		const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS70_P5rBBdArG539fGuRO3zS_Wt2Pm71_FeMwjQGBG-8iqGq9apAGMJuahBuMnRN_KpzPVc6R_lKSq/pub?output=csv';
-		const request = new Request(sheetURL);
-		fetch(request).then(response => {
-			if(response.status === 200){
-				return response.text();
-			}
-			else{
-				throw new Error('Something went wrong getting the data from Google! :c')
-			}
-		}).then(async response => {
-			const artists = response.split('\r\n');
-			artists.shift(); //First row is a header, so lets remove that
-			const problematicArtists = await getArtists();
-			
-			const problematicArtistCount = problematicArtists.filter(artist => artists.includes(artist)).length;
-			
-			const span = document.createElement('span');
-			span.innerText = ' • ' + problematicArtistCount + ' problematic artist' + (problematicArtistCount === 1 ? '' : 's');
-			
-			document.querySelector('.header-scrobble-since').after(span);
-		});
+		if(document.querySelector('.header-scrobble-since')){
+			const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS70_P5rBBdArG539fGuRO3zS_Wt2Pm71_FeMwjQGBG-8iqGq9apAGMJuahBuMnRN_KpzPVc6R_lKSq/pub?output=csv';
+			const request = new Request(sheetURL);
+			fetch(request).then(response => {
+				if(response.status === 200){
+					return response.text();
+				}
+				else{
+					throw new Error('Something went wrong getting the data from Google! :c')
+				}
+			}).then(async response => {
+				const artists = response.split('\r\n');
+				artists.shift(); //First row is a header, so lets remove that
+				const problematicArtists = await getArtists();
+				
+				const problematicArtistCount = problematicArtists.filter(artist => artists.includes(artist)).length;
+				
+				const span = document.createElement('span');
+				span.innerText = ' • ' + problematicArtistCount + ' problematic artist' + (problematicArtistCount === 1 ? '' : 's');
+				
+				document.querySelector('.header-scrobble-since').after(span);
+			});
+		}
 	}
 	
 	main();
